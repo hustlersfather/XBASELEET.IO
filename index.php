c8d5a1cbc0aee2ba9c1683e78a42fea9cbe65e71
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-22 06:55:05 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+ob_start();
+session_start();
+date_default_timezone_set('UTC');
+include "includes/config.php";
+
+if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
+    header("location: login");
+    exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+?>
+
+
+
+
 
 <!DOCTYPE html>
 <html>
