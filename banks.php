b20612e653f799dc768463da808c2892e4b8241b
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-22 23:17:09 -0800

    Update banks.php

--- banks.php
+++ banks.php
@@ -1,78 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+        <title>Fetch Data From MySQL Database using AJAX in PHP</title>
+    </head>
+    <body>
+        <div class="container">
+            <h4>User data</h4>
+            <table class="table">
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Created at</th>
+                </tr>
+                <tbody id="data">
 
-<?php
-}
- else if($_GET['show']=="banks"){
+                </tbody>
+            </table>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript">
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "data.php",
+        dataType: "html",
+        success: function (data) {
+            $("#data").html(data);
 
- ?>
-<table width="100%" id="dataTable" class="table table-striped table-bordered table-condensed sticky-header dataTable no-footer" role="grid" aria-describedby="dataTable_info" style="width: 100%;">       
- <thead>
-  <thead>
-  <tr>
-  <th>ID</th>
-  <th>Seller</th>
-  <th>Country</th>
-  <th>Bank Name</th>
-  <th>Balance</th>
-  <th>Information</th>
-  <th></th>
-  <th>Date added</th>
-  <th>Price</th>
-  <th>Action</th>
-  </tr>
-        </thead>
-		     <tbody id='tbody2'>
- <?php
-$uid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
-$qu = mysqli_query($dbcon, "SELECT * FROM banks WHERE acctype='banks' and sold='0' ORDER BY id DESC")or die(mysqli_error());
-
- while($row = mysqli_fetch_assoc($qu)){
-	 
-    echo "<tr class='banks_darta'>
-    <td> ".htmlspecialchars($row['id'])." </td>
-    <td> ".htmlspecialchars($row['country'])." </td>
-    <td> ".htmlspecialchars($row['balance'])." </td>
-    <td> ".htmlspecialchars($row['infos'])." </td>
-    <td> ".htmlspecialchars($row['bankname'])." </td>
-	td> ".strtoupper(htmlspecialchars($row['resseller']))." </td><
-	<td>  "; ?>
-	<a data-toggle="modal" class="btn btn-primary btn-xs" data-target="#myModald<?php echo  $row['id']; ?>" >
-<font color=white>Open #<?php echo htmlspecialchars($row['id']); ?> </a></center> 
-
-
-
-
-<?php
-  echo '
- 
-<div class="modal fade" id="myModald' . $row['id'] . '" >
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h4 class="modal-title" id="myModalLabel">
-                                           <font color="black"> Banks #' . $row['id'] . ' </font>
-                                            </h4>
-                                        </div>
-                                        <div class="modal-body">
-					<font color="black">			'.htmlspecialchars($row['url']).' </font>
-					</div>								
-					<div class="modal-footer">
-<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-</div>';
-echo "
-</td>
-    <td> ".htmlspecialchars($row['date'])." </td>
-    <td> ".htmlspecialchars($row['price'])."</td>
-    <td> ";
-if ($row['sold'] == "0") {
- echo '<div id="shop'.$row["id"].'" type="Buy"><a onclick="javascript:deletbanks('.$row["id"].');" class="btn btn-danger btn-xs">remove</a></div>';
- }elseif ($row['sold'] == "deleted") {
-	echo "<font color=gray>Deleted</font>"; } else {
-echo "<font color=green>[Sold]</font>";	    
-	}
-    echo "</td>
-    </tr>";
- }
-
- 
-
- ?>
+        }
+    });
+});
+	    </script>
+	</body>
+</html>
