a55eb3866f50204afc60e9bc94337bcc0ca5cf7b
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-24 04:35:37 -0800

    Update leads.php

--- leads.php
+++ leads.php
@@ -419,11 +419,101 @@ Send
 </div>
 </nav>
 
+<?php
+ob_start();
+session_start();
+date_default_timezone_set(‘UTC’);
+include “../includes/config.php”;
+
+if (!isset($_SESSION[‘sname’]) and !isset($_SESSION[‘spass’])) {
+    header(“location: ../“);
+    exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION[‘sname’]);
+?>
+
+<ul class=“nav nav-tabs”>
+  <li class=“active”><a href=“#filter” data-toggle=“tab”>Filter</a></li>
+</ul>
+<div id=“myTabContent” class=“tab-content” >
+  <div class=“tab-pane active in” id=“filter”><table class=“table”><thead><tr><th>Country</th>
+<th>Description</th>
+<th>Seller</th>
+<th></th></tr></thead><tbody><tr><td><select class=‘filterselect form-control input-sm’ name=“leads_country”><option value=“”>ALL</option>
+<?php
+$query = mysqli_query($dbcon, “SELECT DISTINCT(`country`) FROM `leads` WHERE `sold` = ‘0’ ORDER BY country ASC”);
+	while($row = mysqli_fetch_assoc($query)){
+	echo ‘<option value=“’.$row[‘country’].’”>’.$row[‘country’].’</option>’;
+	}
+?>
+</select></td><td><input class=‘filterinput form-control input-sm’ name=“leads_about” size=‘3’></td><td><select class=‘filterselect form-control input-sm’ name=“leads_seller”><option value=“”>ALL</option>
+<?php
+$query = mysqli_query($dbcon, “SELECT DISTINCT(`resseller`) FROM `leads` WHERE `sold` = ‘0’ ORDER BY resseller ASC”);
+	while($row = mysqli_fetch_assoc($query)){
+		 $qer = mysqli_query($dbcon, “SELECT DISTINCT(`id`) FROM resseller WHERE username=‘”.$row[‘resseller’].”’ ORDER BY id ASC”)or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = “seller”.$rpw[“id”].””;
+	echo ‘<option value=“’.$SellerNick.’”>’.$SellerNick.’</option>’;
+	}
+?>
 
 
 
 
-              </td>
+
+
+
+
+
+
+</select></td><td><button id=‘filterbutton’class=“btn btn-primary btn-sm” disabled>Filter <span class=“glyphicon glyphicon-filter”></span></button></td></tr></tbody></table></div>
+</div>
+
+
+<table width=“100%”  class=“table table-striped table-bordered table-condensed sticky-header” id=“table”>
+<thead>
+    <tr>
+      <th scope=“col” >Country</th>
+      <th scope=“col”>Description</th>
+      <th scope=“col”>Email N</th>
+
+      <th scope=“col”>Seller</th>
+      <th scope=“col”>Price</th>
+      <th scope=“col”>Added on </th>
+
+      <th scope=“col”>Buy</th>
+    </tr>
+</thead>
+  <tbody>
+
+ <?php
+include(“cr.php”);
+$q = mysqli_query($dbcon, “SELECT * FROM leads WHERE sold=‘0’ ORDER BY RAND()”)or die(mysqli_error());
+ while($row = mysqli_fetch_assoc($q)){
+	 
+	 	 $countryfullname = $row[‘country’];
+	  $code = array_search(“$countryfullname”, $countrycodes);
+	 $countrycode = strtolower($code);
+	    $qer = mysqli_query($dbcon, “SELECT * FROM resseller WHERE username=‘”.$row[‘resseller’].”’”)or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = “seller”.$rpw[“id”].””;
+     echo “
+ <tr>     
+    <td id=‘leads_country’><i class=‘flag-icon flag-icon-$countrycode’></i>&nbsp;”.htmlspecialchars($row[‘country’]).” </td>
+    <td id=‘leads_about’> “.htmlspecialchars($row[‘infos’]).” </td> 
+	<td> “.htmlspecialchars($row[‘number’]).” </td>
+    <td id=‘leads_seller’> “.htmlspecialchars($SellerNick).”</td>
+    <td> “.htmlspecialchars($row[‘price’]).”</td>
+	    <td> “.$row[‘date’].”</td>”;
+    echo ‘
+    <td>
+	<span id=“leads’.$row[‘id’].’” title=“buy” type=“leads”><a onclick=“javascript:buythistool(‘.$row[‘id’].’)” class=“btn btn-primary btn-xs”><font color=white>Buy</font></a></span><center>
+    </td>
+         
+ 
+
+
+
                                   </tr>
                                   <?php
 
@@ -435,7 +525,38 @@ Send
                               ?>
                             </tbody>
                           </table>
+<div class=“modal fade” id=“myModal” tabindex=“-1” role=“dialog” aria-labelledby=“myModalLabel”>
+  <div class=“modal-dialog” role=“document”>
+    <div class=“modal-content”>
+      <div class=“modal-header”>
+        <button type=“button” class=“close” data-dismiss=“modal” aria-label=“Close”><span aria-hidden=“true”>&times;</span></button>
+        <h4 class=“modal-title” id=“myModalLabel”></h4>
+      </div>
+      <div class=“modal-body” id=“modelbody”>
 
+
+      </div>
+      <div class=“modal-footer”>
+        <button type=“button” class=“btn btn-primary” data-dismiss=“modal”>Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+=
+function openitem(order){
+  $(“#myModalLabel”).text(‘Order #’+order);
+  $(‘#myModal’).modal(‘show’);
+  $.ajax({
+    type:       ‘GET’,
+    url:        ‘showOrder’+order+’.html’,
+    success:    function(data)
+    {
+        $(“#modelbody”).html(data).show();
+    }});
+
+}
 
 </body>
 </html>
