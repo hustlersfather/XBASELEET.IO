6b863b478fcc9b3db62da357b4f68401d14a143d
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-24 04:50:19 -0800

    Update leads.php

--- leads.php
+++ leads.php
@@ -1,7 +1,4 @@
 
-Raw Blame
-
-
 <?php
 ob_start();
 session_start();
@@ -442,22 +439,17 @@ if ($reselerif == "1") {
 </div>
 </nav>
 <style>
-
-<ul class="nav nav-tabs">
-  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
-</ul>
-<div id="myTabContent" class="tab-content" >
-  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
-<th>Description</th>
-<th>Seller</th>
-<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="leads_country"><option value="">ALL</option>
+<Dv I'd="mainDiv>
 <?php
 $query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `leads` WHERE `sold` = '0' ORDER BY country ASC");
 	while($row = mysqli_fetch_assoc($query)){
 	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
 	}
 ?>
-</select></td><td><input class='filterinput form-control input-sm' name="leads_about" size='3'></td><td><select class='filterselect form-control input-sm' name="leads_seller"><option value="">ALL</option>
+
+
+
+
 <?php
 $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `leads` WHERE `sold` = '0' ORDER BY resseller ASC");
 	while($row = mysqli_fetch_assoc($query)){
