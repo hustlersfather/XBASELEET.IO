c65b1b3bf69579be8b717a689e3800a6c0fd5d41
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 08:48:01 +0100

    Update leads.php

--- leads.php
+++ leads.php
@@ -528,3 +528,48 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `leads` WHERE `
 </tr>
 	</thead>
   <tbody>
+ <?php
+include("cr.php");
+$q = mysqli_query($dbcon, "SELECT * FROM leads WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
+ while($row = mysqli_fetch_assoc($q)){
+	 
+	 	 $countryfullname = $row['country'];
+	  $code = array_search("$countryfullname", $countrycodes);
+	 $countrycode = strtolower($code);
+	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = "seller".$rpw["id"]."";
+   
+	 
+	 echo "
+
+<tr>     
+                                <td id="leads'.$row['id'].'" </td>
+   
+ <td id='leads_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
+          
+	  
+	  
+	  <td id='leads_about'> ".htmlspecialchars($row['infos'])." </td> 
+
+
+             <td> ".htmlspecialchars($row['number'])." </td>
+     
+     
+       <td id='leads_seller'> ".htmlspecialchars($SellerNick)."</td>
+      
+      
+           <td> ".htmlspecialchars($row['price'])."</td>
+	 
+	 
+	 
+	            <td> ".$row['date']."</td>";
+    echo '
+    <td>
+	<span id="leads'.$row['id'].'" title="buy" type="leads"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
+    </td>
+            </tr>
+     ';
+ }
+
+ ?>
