88a900e5ed98f7bdb68f949e0eb1b1b7b1fdbef0
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 12:11:25 +0100

    Update leads.php

--- leads.php
+++ leads.php
@@ -537,7 +537,7 @@ a.closearb {
 </div>
 <div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
 <div class="col-sm-12 table-responsive">
-<table id="table" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
+<table id="lead_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
 <thead>
 <tr>
 <th data-priority="1"></th>
