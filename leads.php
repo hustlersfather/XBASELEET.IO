8f8f5cd38f5a03fa22356e76c0e71f6044f41dfb
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 10:15:36 +0100

    Update leads.php

--- leads.php
+++ leads.php
@@ -11,34 +11,44 @@ if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
 }
 $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 ?>
-
-<ul class="nav nav-tabs">
-  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
-</ul>
-<div id="myTabContent" class="tab-content" >
-  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
-<th>Description</th>
-<th>Seller</th>
-<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="leads_country"><option value="">ALL</option>
-<?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `leads` WHERE `sold` = '0' ORDER BY country ASC");
-	while($row = mysqli_fetch_assoc($query)){
-	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
-	}
-?>
-</select></td><td><input class='filterinput form-control input-sm' name="leads_about" size='3'></td><td><select class='filterselect form-control input-sm' name="leads_seller"><option value="">ALL</option>
-<?php
-$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `leads` WHERE `sold` = '0' ORDER BY resseller ASC");
-	while($row = mysqli_fetch_assoc($query)){
-		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-	}
-?>
-</select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
+</style> <div class="d-flex flex-row-reverse mt-0">
+<div class="p-2">
+<label id="switch" class="switch">
+<input type="checkbox" onchange="toggleTheme()" id="slider">
+<span class="slider round">
+</span>
+</label>
 </div>
-<div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);"><div class="col-sm-12 table-responsive">
+</div>
+<div class="alert alert-info text-left" role="alert" style="margin: 15px;">
+<ul>
+<li>We Have Fresh E-mails Lists for Diffrents Countries ( You Can Use it for Spam ). </li>
+<li>Our E-mails Lists Guarante for you Good results because most of them are from Shops and Big Sites .</li>
+<li>Combo use for cracking (Brute forcing attack) not use for login to email.</li>
+<li>There is <b> 2 </b> Available.</li>
+</ul>
+</div>
+<input type=hidden id="type" name="type" value="4" />
+<div class="row m-3 pt-1" style="color: var(--font-color);">
+<div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+ <label for="infos" style="margin-bottom: 10px; margin-top: 5px">Description:</label>
+<input type="search" class="form-control" id="infos" style="color: var(--font-color); background-color: var(--color-card);">
+</div>
+<div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+<label for="Country" style="margin-bottom: 10px; margin-top: 5px">Country :</label>
+<select name="country" id="country" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+<option value="">All Countries</option>
+<option value="Canada">Canada</option><option value="France">France</option> </select>
+</div>
+<div class="col-xs-6 col-sm-4 col-lg-2" style="display:inline-block">
+<label for="seller" style="margin-bottom: 10px; margin-top: 5px">Seller :</label>
+<select name="seller" id="seller" class="form-control" style="color: var(--font-color); background-color: var(--color-card);">
+<option value="">All</option>
+<option value="Seller467">Seller467</option> </select>
+</div>
+</div>
+<div class="row m-2 pt-3 " style="max-width:100%; color: var(--font-color); background-color: var(--color-card);">
+<div class="col-sm-12 table-responsive">
 <table id="lead_data" class="display responsive table-hover" style="width:100%; color: var(--font-color); background-color: var(--color-card);">
 <thead>
 <tr>
@@ -55,96 +65,197 @@ $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `leads` WHERE `
 </tr>
 </thead>
 </table>
-			 <?php
-include("cr.php");
-$q = mysqli_query($dbcon, "SELECT * FROM leads WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
- while($row = mysqli_fetch_assoc($q)){
-	 
-	 	 $countryfullname = $row['country'];
-	  $code = array_search("$countryfullname", $countrycodes);
-	 $countrycode = strtolower($code);
-	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-     echo "	
-		 <tr>
-                                
-					 <td id='leads_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
-			
-					  <td id='leads_about'> ".htmlspecialchars($row['infos'])." </td>
-					  
-					  <td> ".htmlspecialchars($row['number'])." </td>
-					  
-					    <td id='leads_seller'> ".htmlspecialchars($SellerNick)."</td>
-					   
-					     <td> ".htmlspecialchars($row['price'])."</td>
-                                 
-					       <td> ".$row['date']."</td>"; 
-                                      
-					  echo '
-                                             <td>
-	                                          <span id="leads'.$row['id'].'" title="buy" type="leads"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
-                                             
-                                       </td>';
-}
+</div>
+</div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<p class="heading" id="myModalHeader"></p>
+<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+<span aria-hidden="true" class="white-text">&times;</span>
+</button>
+</div>
+<div class="modal-body" id="modelbody">
+</div>
+<div class="modal-footer justify-content-center">
+<a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">Close</a>
+</div>
+</div>
+</div>
+</div>
+
+<div class="modal fade" id="modalConfirmBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered modal-sm modal-notify modal-info" role="document">
+
+<div class="modal-content text-center">
+
+<div class="modal-header d-flex justify-content-center">
+<p class="heading">Are you sure?</p>
+</div>
+
+<div class="modal-body">
+<i class='fas fa-shopping-cart fa-4x animated rotateIn'></i>
+</div>
+
+<div class="modal-footer flex-center">
+<a onClick='confirmbye()' class="btn btn-outline-info waves-effect" data-dismiss="modal">Yes</a>
+<a type="button" class="btn btn-info" data-dismiss="modal">No</a>
+</div>
+</div>
+
+</div>
+</div>
+
+
+<div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
+<div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
+
+<div class="modal-content">
+
+<div class="modal-body">
+<div class="row d-flex justify-content-center align-items-center">
+<img src="layout/images/balance.png">
+<span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
+<a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
+<i class="fas fa-book ml-1 white-text"></i>
+</a>
+<a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No, thanks</a>
+</div>
+</div>
+</div>
+
+</div>
+</div>
+<script>
+	$(document).ready(function(){
+            var webID;
+            load_data();
+
+            function load_data(myarray) {
+                $('#lead_data').DataTable({
+                    "processing": true,
+                    "serverSide": true,
+                    "responsive": true,
+                    "scrollX": true,
+                    "order": [],
+                    "lengthMenu": [[10, 25, 50, 100, 500, 10000], [10, 25, 50, 100, 500, "All"]],
+                    "columnDefs": [
+                        {
+                            "targets": [ 0 ],
+                            "visible": false
+                        }
+                    ],
+
+                    "ajax":{
+                        url:"divPage6.html",
+                        type:"POST",
+                        data:{
+                            data_filter:myarray,
+                            type:document.getElementById('type').value,
+                             draw : 'draw',
+                             row : 'start',
+                             rowperpage : 'length',
+                             columnIndex : 'order',
+                             columnName : 'columns',
+                             columnSortOrder : 'order',
+                             searchValue : 'search'
+                              }
+                    },
+                 "columns": [
+                                { "data": 0 },
+                                { "data": 1 },
+                                { "data": 2 },
+                                { "data": 3 },
+                                { "data": 4 },
+                                { "data": 5 },
+                                { "data": 6 },
+                                { "data": 7 },
+                                { "data": 8 },
+                                { "data": 9 }
+                            ],
+
+                    "pageLength": 500
+                });
+            }
+
+            $(document).on('change', '.form-control', function(){
+                $('#lead_data').DataTable().destroy();
+                 var country = $('#country').val();
+                var description = $('#infos').val();
+                 var seller1 = $('#seller').val();
+                $idseller = seller1.split("Seller");
+                 var seller= $idseller[1];
+                 var myarray = {};
+
+                 myarray[0] = country;
+                 myarray[1] = description;
+                 myarray[2] = seller;
+
+              if(country != '' || description != '' || seller != '')
+                {
+
+                   load_data(myarray);
+                }
+                else
+                {
+                    load_data();
+                }
+                });
+
+
+        });
+
+       function buythistool(id){
+         $('#modalConfirmBuy').modal('show');
+         webID= id;
+                        }
+
+        function confirmbye(id){
+              id= webID;
+            $.ajax({
+                        method:"GET",
+                        url:"buytool.php?id="+id+"&t=leads",
+                        dataType:"text",
+                        success:function(data){
+                                if(data.match("buy")){
+                                let lastid = data.split("buy,")[1];
+                                $("#lead"+id).html(`<button onclick=openitem(${lastid}) class="btn btn-success btn-sm">Order ${'#'+lastid}</button>`).show();
+
+                            }
+                            else{
+                                if(data.match("deleted")){
+
+                                $("#lead"+id).html('Already sold / Deleted').show();
+
+                                  }else{
+                                   $('#modalCoupon').modal('show');
                                 }
-                                else{
-                                  echo "NO RECORD FOUND";
-                                }
-                              ?>
-                            </tbody>
-                        </table>
-			<script type="text/javascript">
-$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#leads_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#leads_about").text().toLowerCase() );var ck3 = $.trim( $(this).find("#leads_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="leads_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="leads_about"]').val().toLowerCase() );var val3 = $.trim( $('select[name="leads_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
-function buythistool(id){
-  bootbox.confirm("Are you sure?", function(result) {
-        if(result ==true){
-      $.ajax({
-     method:"GET",
-     url:"buytool.php?id="+id+"&t=leads",
-     dataType:"text",
-     success:function(data){
-         if(data.match(/<button/)){
-		 $("#leads"+id).html(data).show();
-         }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
-         }
-     },
-   });
-       ;}
-  });
-}
-function openitem(order){
-  $("#myModalLabel").text('Order #'+order);
-  $('#myModal').modal('show');
-  $.ajax({
-    type:       'GET',
-    url:        'showOrder'+order+'.html',
-    success:    function(data)
-    {
-        $("#modelbody").html(data).show();
-    }});
-
-}
-
-</script>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel"></h4>
-      </div>
-      <div class="modal-body" id="modelbody">
 
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>		  
-					 
+                            }
+                        },
+                    });
+        }
+    function openitem(order){
+
+
+        $.ajax({
+        type:       'GET',
+        url:        'showOrder'+order,
+        success:    function(data)
+        {
+        $("#myModalHeader").text('Order #'+order);
+        $("#modelbody").append(data);
+        $('#myModal').modal();
+
+
+        }});
+
+        }
+
+    </script>
+</body>
+</html>
 					  
 				
