31c6e572b8cc1426a8b5613fbb68da1431889f9c
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-26 21:27:36 +0100

    Update banks.php

--- banks.php
+++ banks.php
@@ -20,8 +20,6 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <link rel="stylesheet" type="text/css" href="files/css/flags.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
 <script type="text/javascript" src="files/bootstrap/3/js/bootstrap.js?1"></script>
-<script type="text/javascript" src="files/js/sorttable.js"></script>
-<script type="text/javascript" src="files/js/table-head.js?3334"></script>
 <script type="text/javascript" src="files/js/bootbox.min.js"></script>
 <script type="text/javascript" src="files/js/clipboard.min.js"></script>
 
@@ -43,13 +41,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <title>JeruxShop</title>
 </head>
 <style>
-#table {
-  .sortable
-}
-table th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sorttable_nosort):after { 
-    content: " \25BE" 
-}
-
+#
 .label-as-badge {
     border-radius: 0.5em;
 }
