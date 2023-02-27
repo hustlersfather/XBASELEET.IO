7d1f2b94218cd3ad556b81de99faab754f638000
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-27 02:05:34 -0800

    Update index.php

--- buyer/index.php
+++ buyer/index.php
@@ -252,32 +252,23 @@ input:checked + .slider:before {
                                                           padding-bottom: 0.5rem;
                                                           padding-left: 1rem;
                                                         ">
-<a class="navbar-brand" href="main" style="color: var(--font-color);"><img width="40px" src="../layout/images/logo.png"> XBASELEET</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-<i class="navbar-toggler-icon"></i>
+<a class=“navbar-brand” href=“main” style=“color: var(—font-color);”><img width=“40px” src=“../layout/images/logo.png”> XBASELEET</a>
+<button class=“navbar-toggler” type=“button” data-toggle=“collapse” data-target=“#navbarSupportedContent” aria-controls=“navbarSupportedContent” aria-expanded=“false” aria-label=“Toggle navigation”>
+<i class=“navbar-toggler-icon”></i>
 </button>
-<div class="collapse navbar-collapse order-1" id="navbarSupportedContent">
-<ul class="navbar-nav mr-auto">
+<div class=“collapse navbar-collapse order-1” id=“navbarSupportedContent”>
+<ul class=“navbar-nav mr-auto”>
 
-<li class="nav-item dropdown mr-auto">
-<a class="nav-link dropdown-toggle" style="color: var(--font-color);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-warehouse fa-sm orange-text"></i>
+<li class=“nav-item dropdown mr-auto”>
+<a class=“nav-link dropdown-toggle waves-effect waves-light” style=“color: var(—font-color);” role=“button” data-toggle=“dropdown” aria-haspopup=“true” aria-expanded=“false”><i class=“fa fa-warehouse fa-sm orange-text”></i>
 Hosts
 </a>
-<div class="dropdown-menu dropdown-menu-left" style="color: var(--font-color); background-color: var(--color-nav);" aria-labelledby="navbarDropdown">
-<a class="dropdown-item" href="rdp" style="color: var(--font-color);"><span class="px-2"><i class="fas fa-desktop fa-fw"></i> RDPs <span class="badge badge-primary"><span id="rdps"></span></span></a>
-<a class="dropdown-item" href="cPanel" style="color: var(--font-color);"><span class="px-2"><i class="fas fa-tools fa-fw"></i> cPanels <span class="badge badge-primary"><span id="cpanels"></span></span></a>
-<a class="dropdown-item" href="shell" style="color: var(--font-color);"><span class="px-2"><i class="fas fa-file-code fa-fw"></i> Shells <span class="badge badge-primary"><span id="shell"></span></span></a>
-<!--<a class="dropdown-item" href="ssh" style="color: var(--font-color);"><span class="px-2"><i class="fab fa-linux"></i> SSH/WHM <span class="badge badge-primary">span id=""></span></span></a>--->
-</div>
-</li>
-<!-----<li class="nav-item dropdown mr-auto">
-<!---<a class="nav-link dropdown-toggle" style="color: var(--font-color);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-leaf fa-fw fa-sm text-success" style="margin-right: 4px;"></i>Premium</a>
-<!----<div class="dropdown-menu dropdown-menu-left" style="color: var(--font-color); background-color: var(--color-nav);" aria-labelledby="navbarDropdown">
-<!----<a class="dropdown-item" href="premium_shell" style="color: var(--font-color);"><span class="px-2"><i class="fas fa-leaf fa-fw"></i> Premium Shells <span class="badge badge-primary">104</span></span></a>
-<!---<a class="dropdown-item" href="premium_cPanel" style="color: var(--font-color);"><span class="px-2"><i class="fas fa-leaf fa-fw"></i> Premium cPanels <span class="badge badge-primary">572</span></span></a>
---->
-</div>
-</li>
+<div class=“dropdown-menu dropdown-menu-left” style=“color: var(—font-color); background-color: var(—color-nav);” aria-labelledby=“navbarDropdown”>
+<a class=“dropdown-item waves-effect waves-light” href=“rdp” style=“color: var(—font-color);”><span class=“px-2”><i class=“fas fa-desktop fa-fw”></i> RDPs <span class=“badge badge-primary”><span id="rdps"></span></span></a>
+<a class=“dropdown-item waves-effect waves-light” href=“cPanel” style=“color: var(—font-color);”><span class=“px-2”><i class=“fas fa-tools fa-fw”></i> cPanels <span class=“badge badge-primary”><span id="cpanels">  </span></span></a>
+<a class=“dropdown-item waves-effect waves-light” href=“shell” style=“color: var(—font-color);”><span class=“px-2”><i class=“fas fa-file-code fa-fw”></i> Shells <span class=“badge badge-primary”> <span id="shell"></span></span></a>
+ </div>
+    </li>
 <li class="nav-item dropdown mr-auto">
 <a class="nav-link dropdown-toggle" style="color: var(--font-color);" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fab fa-google-play fa-sm text-success"></i>
 Send
