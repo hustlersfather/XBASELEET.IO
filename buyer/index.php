65d706c76f20a6f1525e55a64a51ccbbad4a39f8
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-26 16:35:43 -0800

    Update index.php

--- buyer/index.php
+++ buyer/index.php
@@ -14,7 +14,7 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 <html>
 <head>
 	
-ink rel="stylesheet" href="../layout/css/bootstrap.min.css" />
+<link rel="stylesheet" href="../layout/css/bootstrap.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />	
 	
 	
